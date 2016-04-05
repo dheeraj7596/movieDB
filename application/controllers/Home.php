a6f7265fc3feea5779334efbc07b881a53f35763
Dheeraj Mekala <dheerajm@iitk.ac.in>
2016-04-06 01:39:49 +0530

    view changed from test.html to movie_genre_page.html

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,18 +7,25 @@ class Home extends CI_Controller {
         public function __construct()
         {
                 parent::__construct();
-                // $this->load->model('books_model');
-                // $this->load->helper('url_helper');
+                $this->load->model('movie_model');
+                $this->load->helper('url_helper');
         }
         public function index()
 	{
-//                $data['bookdetails'] = $this->books_model->get_books();
+
 //                $data['title'] = 'Books archive';
 //		$this->load->view('template/header',$data);
 //		$this->load->view('displayBook',$data);
 //		$this->load->view('template/footer');
-                $this->load->view('index.html');
+                $this->load->view('index.php');
 	}
+            public function genre($genre)
+        {
+                $data['movieDetails'] = $this->movie_model->get_movie_by_genre($genre);
+                $data['genre'] = $genre;
+                $this->load->view('movie_genre_page.html',$data);
+//                echo implode(" ",$data['bookdetails'][1]);
+        }
 
 
 }
