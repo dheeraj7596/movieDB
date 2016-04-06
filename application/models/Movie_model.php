2c5cd9d0456c56d385f23cff13906a0e84fc5bb8
Preetansh <preetg@iitk.ac.in>
2016-04-07 02:47:10 +0530

    some progress in auth

--- application/models/Movie_model.php
+++ application/models/Movie_model.php
@@ -45,7 +45,7 @@ class Movie_model extends CI_Model {
         //         $query = $this->db->get();
         //         return $query->result_array();
         // }
-        
+
         // public function get_author_books($author = FALSE)
         // {
         //         if ($author === FALSE)
@@ -59,7 +59,7 @@ class Movie_model extends CI_Model {
         //         $query = $this->db->get();
         //         return $query->result_array();
         // }
-        
+
         // public function get_author_info($author = FALSE)
         // {
         //         if ($author === FALSE)
@@ -73,5 +73,60 @@ class Movie_model extends CI_Model {
         //         $query = $this->db->get();
         //         return $query->result_array();
         // }
-}
 
+        // Insert registration data in database
+        public function registration_insert($data) {
+
+            // Query to check whether username already exist or not
+            $condition = "username =" . "'" . $data['username'] . "'";
+            $this->db->select('*');
+            $this->db->from('test');
+            $this->db->where($condition);
+            $this->db->limit(1);
+            $query = $this->db->get();
+            if ($query->num_rows() == 0) {
+
+            // Query to insert data in database
+            $this->db->insert('usertest', $data);
+            if ($this->db->affected_rows() > 0) {
+            return true;
+            }
+            } else {
+            return false;
+            }
+        }
+
+        // Read data using username and password
+        public function login($data) {
+
+            $condition = "username =" . "'" . $data['username'] . "' AND " . "password =" . "'" . $data['password'] . "'";
+            $this->db->select('*');
+            $this->db->from('test');
+            $this->db->where($condition);
+            $this->db->limit(1);
+            $query = $this->db->get();
+
+            if ($query->num_rows() == 1) {
+            return true;
+            } else {
+            return false;
+            }
+        }
+
+        // Read data from database to show data in admin page
+        public function read_user_information($username) {
+
+            $condition = "username =" . "'" . $username . "'";
+            $this->db->select('*');
+            $this->db->from('test');
+            $this->db->where($condition);
+            $this->db->limit(1);
+            $query = $this->db->get();
+
+            if ($query->num_rows() == 1) {
+            return $query->result();
+            } else {
+            return false;
+            }
+        }
+}
