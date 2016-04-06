eee3a3c27ca38cbb54288ff62c65dac5c99186a1
Dheeraj Mekala <dheerajm@iitk.ac.in>
2016-04-07 03:05:38 +0530

    minor changes

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,18 +7,44 @@ class Home extends CI_Controller {
         public function __construct()
         {
                 parent::__construct();
-                // $this->load->model('books_model');
-                // $this->load->helper('url_helper');
+                $this->load->model('movie_model');
+                $this->load->helper('url_helper');
         }
         public function index()
 	{
-//                $data['bookdetails'] = $this->books_model->get_books();
+
 //                $data['title'] = 'Books archive';
 //		$this->load->view('template/header',$data);
 //		$this->load->view('displayBook',$data);
 //		$this->load->view('template/footer');
-                $this->load->view('index.html');
+                $this->load->view('index.php');
 	}
+            public function genre($genre)
+            {
+                $data['movieDetails'] = $this->movie_model->get_movie_by_genre($genre);
+                $data['genre'] = $genre;
+                $this->load->view('movie_genre_page.html',$data);
+//                echo implode(" ",$data['bookdetails'][1]);
+            }
 
+            public function review_movie($movieid)
+            {
+                $data['reviews'] = $this->movie_model->get_review_by_movie($movieid);
+                $this->load->view('movie_review.html',$data);
+            }
+
+            public function read_review($reviewid)
+            {
+                $data['read'] = $this->movie_model->get_full_review($reviewid);
+                $this->load->view('full_review.html',$data);
+            }
+            public function write_review($asd)
+            {
+                $dbconnect = $this->load->database();
+                $this->load->library('form_validation');
+                $this->load->model('movie_model');
+
+            }
+            // public function review_submit($page = 'insert')
 
 }
---
+++ application/views/write_review.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Cart | E-Shopper</title>
+    <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/css/prettyPhoto.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/css/price-range.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/css/animate.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/css/main.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/css/responsive.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="assets/js/html5shiv.js"></script>
+    <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+    <link rel="shortcut icon" href="<?php echo base_url();?>assets/images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url();?>assets/images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url();?>assets/images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url();?>assets/images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo base_url();?>assets/images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+
+<body>
+    <?=form_open('insert/data_submit/'); ?>
+    <table width="485" cellspacing="0" cellpadding="0">
+        <tr>
+            <td>Title:</td>
+            <td><input name="title" value="<?=set_value('title');?>" type="text" /></td>
+            <td><?=form_error('title');?></td>
+        </tr>
+        <tr>
+            <td>Body:</td>
+            <td><input name="body" value="<?=set_value('body');?>" type="text" /></td>
+            <td><?=form_error('body');?></td>
+        </tr>
+    </table>
+    <?=form_close();?>
+
+
+
+
+
+</body>
