ceb5a52714e4d6e25adbb24d65475c0ad8871da6
Preetansh <preetg@iitk.ac.in>
2016-04-11 00:08:09 +0530

    login page changed

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -26,7 +26,8 @@ class Home extends CI_Controller {
 //		$this->load->view('template/header',$data);
 //		$this->load->view('displayBook',$data);
 //		$this->load->view('template/footer');
-                $this->load->view('login_form.php');
+                // $this->load->view('login_form.php');
+                $this->load->view('login.html');
 	}
             public function genre($genre)
             {
@@ -107,7 +108,7 @@ class Home extends CI_Controller {
             // $this->form_validation->set_rules('email_value', 'Email', 'trim|required|xss_clean');
             $this->form_validation->set_rules('password', 'Password', 'trim|required');
             if ($this->form_validation->run() == FALSE) {
-                $this->load->view('registration_form');
+                $this->load->view('login.html');
             } else {
                 $data = array(
                 'username' => $this->input->post('username'),
@@ -118,10 +119,10 @@ class Home extends CI_Controller {
                 $result = $this->movie_model->registration_insert($data);
                 if ($result == TRUE) {
                     $data['message_display'] = 'Registration Successfully !';
-                    $this->load->view('login_form', $data);
+                    $this->load->view('login.html', $data);
                 } else {
                     $data['message_display'] = 'Username already exist!';
-                    $this->load->view('registration_form', $data);
+                    $this->load->view('login.html', $data);
                 }
             }
         }
@@ -136,7 +137,7 @@ class Home extends CI_Controller {
                 if(isset($this->session->userdata['logged_in'])){
                     $this->load->view('index');
                 }else{
-                    $this->load->view('login_form');
+                    $this->load->view('login.html');
                 }
             } else {
                 $data = array(
@@ -162,7 +163,7 @@ class Home extends CI_Controller {
                     $data = array(
                     'error_message' => 'Invalid Username or Password'
                     );
-                    $this->load->view('login_form', $data);
+                    $this->load->view('login.html', $data);
                 }
             }
         }
@@ -177,7 +178,7 @@ class Home extends CI_Controller {
             );
             $this->session->unset_userdata('logged_in', $sess_array);
             $data['message_display'] = 'Successfully Logout';
-            $this->load->view('login_form', $data);
+            $this->load->view('login.html', $data);
         }
 
         public function movie_info_name()
